d4c8d939ee75805aee660340a048dd4ac4e1fdea
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2022-11-21 16:44:15 +0100

    feat: create todo

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -9,4 +9,42 @@ const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
 
-// TODO: Implement the dataLayer logic
+export class TodosAccess {
+
+    constructor(
+      private readonly docClient: DocumentClient = createDynamoDBClient(),
+      private readonly groupsTable = process.env.TODOS_TABLE) {
+    }
+  
+    async getAllItems(): Promise<TodoItem[]> {
+      console.log('Getting all items')
+  
+      const result = await this.docClient.scan({
+        TableName: this.groupsTable
+      }).promise()
+  
+      const items = result.Items
+      return items as TodoItem[]
+    }
+  
+    async createTodItem(item: TodoItem): Promise<TodoItem> {
+      await this.docClient.put({
+        TableName: this.groupsTable,
+        Item: group
+      }).promise()
+  
+      return item
+    }
+  }
+  
+  function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+      console.log('Creating a local DynamoDB instance')
+      return new XAWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      })
+    }
+  
+    return new XAWS.DynamoDB.DocumentClient()
+  }
--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { JwtToken } from '../../utils/JwtToken'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-bpy07h3m6yf2qo7t.us.auth0.com/.well-known/jwks.json';
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +61,13 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  
+    const response = await Axios.get(jwksUrl);
+    console.log(response);
+    const cert ="";
+    //certToPEM(response.n);
+
+     return verify(token, cert, { algorithms: ['RS256'] }) as JwtToken
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +80,10 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+export function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
